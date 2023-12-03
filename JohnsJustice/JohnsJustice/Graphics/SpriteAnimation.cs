3a37120f4594b376da624acf933f50b6606e5a1a
Jacob Kirkbride <jakelegendxiii@gmail.com>
2023-12-03 00:08:08 -0500

    Add starter animation code

---
+++ JohnsJustice/JohnsJustice/Graphics/SpriteAnimation.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JohnsJustice.Graphics
+{
+	public class SpriteAnimation
+	{
+		private List<SpriteAnimationFrame> _frames = new List<SpriteAnimationFrame>();
+
+		public SpriteAnimationFrame this[int index]
+		{
+			get => GetFrame(index);
+		}
+
+		public bool IsPlaying { get; private set; }
+		public float PlaybackProgress { get; private set; }
+		public bool ShouldLoop { get; set; } = true;
+
+		public SpriteAnimationFrame CurrentFrame
+		{
+			get
+			{
+				return _frames
+					.Where(f => f.TimeStamp <= PlaybackProgress)
+					.OrderBy(f => f.TimeStamp).LastOrDefault();
+			}
+		}
+
+		public float Duration => _frames.Count > 0 ? _frames.Max(f => f.TimeStamp) : 0;
+
+		public void AddFrame(Sprite sprite, float timeStamp)
+		{
+			_frames.Add(new SpriteAnimationFrame(sprite, timeStamp));
+		}
+
+		public void Update(GameTime gameTime)
+		{
+			if (IsPlaying)
+			{
+				PlaybackProgress += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+				if (PlaybackProgress > Duration)
+				{
+					if (ShouldLoop)
+					{
+						PlaybackProgress -= Duration;
+					}
+					else
+					{
+						Stop();
+					}
+				}
+			}
+		}
+
+		public void Draw(SpriteBatch spriteBatch, Vector2 position)
+		{
+			SpriteAnimationFrame frame = CurrentFrame;
+
+			if (frame != null)
+			{
+				frame.Sprite.Draw(spriteBatch, position);
+			}
+		}
+
+		public void Play()
+		{
+			IsPlaying = true;
+		}
+
+		public void Stop()
+		{
+			IsPlaying = false;
+			PlaybackProgress = 0;
+		}
+
+		public SpriteAnimationFrame GetFrame(int index)
+		{
+			if (index < 0 || index >= _frames.Count)
+			{
+				throw new ArgumentOutOfRangeException(nameof(index), "The index must be within the bounds of the frames list.");
+			}
+
+			return _frames[index];
+		}
+
+		public void Clear()
+		{
+
+			Stop();
+			_frames.Clear();
+		}
+	}
+}
---
+++ JohnsJustice/JohnsJustice/Graphics/SpriteAnimationFrame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace JohnsJustice.Graphics
+{
+	public class SpriteAnimationFrame
+	{
+		private Sprite _sprite;
+		public Sprite Sprite
+		{
+			get => _sprite;
+			set
+			{
+				if (value == null)
+				{
+					throw new ArgumentNullException("value", "The sprite cannot be null.");
+				}
+
+				_sprite = value;
+			}
+		}
+
+		public float TimeStamp { get; }
+
+		public SpriteAnimationFrame(Sprite sprite, float timeStamp)
+		{
+			Sprite = sprite;
+			TimeStamp = timeStamp;
+		}
+	}
+}
