f5f40a369547354b6ea018cb003f58de1a98ab50
Jacob Kirkbride <jakelegendxiii@gmail.com>
2023-12-03 00:02:50 -0500

    Start adding entities

---
+++ JohnsJustice/JohnsJustice/Entities/IGameEntity.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace JohnsJustice.Entities
+{
+	internal interface IGameEntity
+	{
+		int DrawOrder { get; }
+		void Update(GameTime gameTime);
+		void Draw(SpriteBatch spriteBatch, GameTime gameTime);
+	}
+}
